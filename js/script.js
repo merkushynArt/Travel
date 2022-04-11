537998c439e96af19aac7394f03694a6f7eccac7
Artem <merkushynletter13@gmail.com>
2022-04-11 14:01:21 +0300

    created a about-section

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@ let navbar = document.querySelector('.header .navbar');
 document.querySelector('#menu-btn').onclick = () => {
    navbar.classList.toggle('active');
 }
+
+document.querySelectorAll('.control-btn').forEach(btn =>{
+   btn.onclick = () => {
+      let src = btn.getAttribute('data-src');
+      document.querySelector('.video').src = src;
+   }
+})
+
